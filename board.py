062752b1c0978bf1173b940864f0f7d581f0fcd3
Will McElhenney <whm0004@auburn.edu>
2022-09-08 17:00:44 -0500

    Made sure that pattern lines were a numpy array

--- board.py
+++ board.py
@@ -8,5 +8,9 @@ class Board:
 	# Define a player's board
 	def __init__(self):
 		self.points = 0
-		self.pattern_lines = [np.zeros(i+1) for i in range(5)]
+		self.pattern_lines = np.array([np.zeros(i+1) for i in range(5)])
 		self.wall = Wall()
+
+if __name__ == '__main__':
+	b = Board()
+	print(b.pattern_lines)
