912115944350aa661aa338aa2a276dad08e3f35e
Will McElhenney <wmcelhenney93@gmail.com>
2022-09-08 23:02:33 -0500

    Implementing board

--- board.py
+++ board.py
@@ -1,16 +1,14 @@
 import numpy as np
 
+from floor_line import FloorLine
 from tile import Tile
+from pattern_lines import PatternLines
 from wall import Wall
 
 class Board:
-
 	# Define a player's board
 	def __init__(self):
 		self.points = 0
-		self.pattern_lines = np.array([np.zeros(i+1) for i in range(5)])
+		self.pattern_lines = PatternLines()
 		self.wall = Wall()
-
-if __name__ == '__main__':
-	b = Board()
-	print(b.pattern_lines)
+		self.floor_line = FloorLine()
