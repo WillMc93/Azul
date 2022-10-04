c7bb689234e0e9fbabaacfdc49aee71fab0d0bbb
Will McElhenney <whm0004@auburn.edu>
2022-10-04 17:00:59 -0500

    Created file for defining the argparser

---
+++ arguments.py
@@ -0,0 +1,9 @@
+import argparse
+
+def init_parser():
+	# Function that defines the parser
+	parser = argparse.ArgumentParser(description="Command Line Interface for Azul Game")
+	parser = add.argument('')
+
+
+	return parser
