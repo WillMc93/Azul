a5604c3259af14c526186ca6c363d7919211e9f9
Will McElhenney <whm0004@auburn.edu>
2022-10-04 16:05:11 -0500

    Added a less_than definition for sorting and function comments

--- tile.py
+++ tile.py
@@ -10,26 +10,36 @@ class Tile:
 	colour: str = ''		
 
 	def __init__(self, colour=''):
+		# Make a Tile if a valid tile colour is provided
 		if colour not in Tile.__TILE_REPS:
 			raise Exception(f"Programming Error: Passed a non-valid label, {colour}, to a Tile object.")
 		self.colour = colour
 
 
-	def __eq__(self, tile):
-		if type(tile) == str:
-			return self.colour == tile
-		return self.colour == tile.colour
+	def __eq__(self, other):
+		# Definite equality between Tiles and Tiles or Tiles and strings
+		if type(other) == str:
+			return self.colour == other
+		return self.colour == other.colour
 
+	def __lt__(self, other)
+		# Definte less_than for Tiles, which is used to sort Tiles by colour
+		return self.colour < other.colour
 
 	def __repr__(self):
+		# Define the representation of Tile objects
 		return f'Tile("{self.colour}")'
 
 
 	def __str__(self):
+		# Define the string representation of Tile objects
 		return Tile.__TILE_REPS[self.colour]
 
 
 	def set(self, colour):
+		# Set the colour of a Tile if it is not already set, which is useful for
+		# creating lists of unset tiles and setting them as necessary.
+		# I think this might be unused
 		if colour not in Tile.__TILE_REPS:
 			raise Exception(f"Programming Error: Passed non-valid colour, {colour}, to Tile.set().")
 		if self.colour != '':
